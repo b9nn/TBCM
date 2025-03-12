0e450e6b20f93c9ceb7f5c9cd19faf347a7face1
bgladney <bgladney@linux.student.cs.uwaterloo.ca>
2025-03-11 23:15:34 -0400

    initializing repo

---
+++ tbcm.py
@@ -0,0 +1,25 @@
+import numpy as np
+# https://numpy.org/doc/2.2/reference/generated/numpy.arange.html#numpy.arange
+
+import matplotlib.pyplot as plt
+# matplot crash course
+# https://www.youtube.com/watch?v=3Xc3CA655Y4
+# documentation ~ https://matplotlib.org/3.5.0/api/_as_gen/matplotlib.pyplot.html
+
+import scipy as sp
+
+from scipy.integrate import solve_ivp 
+# scipy resources:
+# https://www.youtube.com/watch?v=MM3cBamj1Ms
+# https://github.com/lukepolson/youtube_channel/blob/main/Python%20Tutorial%20Series/odes1.ipynb
+
+# sample langrangian mechanics problem
+# https://www.youtube.com/watch?v=rJaXxb_piGI
+
+
+import pandas as pd
+# https://pandas.pydata.org/docs/
+
+
+import sympy as smp # allows for symbolic manipulation
+
