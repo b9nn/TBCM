650cd2164c227c4d11fc0ba5311f6f47a7f7f2fb
bgladney <bgladney@linux.student.cs.uwaterloo.ca>
2025-03-12 20:10:17 -0400

    changing computers

--- tbcm.py
+++ tbcm.py
@@ -1,3 +1,19 @@
+'''
+The Triangular Body-Cover Model (TBCM) is a computational model used to simulate voice production, 
+ in the context of vocal hyperfunction and phonotrauma. Expanding on the classic body-cover 
+model (BCM), the TBCM incorporates a triangular glottal shape. This model allows for the simulation of
+laryngal mechanisms. 
+
+Coded by Benjamin Gladney
+March 11th 2025
+
+            --- References ---
+[1] 
+
+
+
+'''
+
 import numpy as np
 # https://numpy.org/doc/2.2/reference/generated/numpy.arange.html#numpy.arange
 
@@ -22,4 +38,28 @@ import pandas as pd
 
 
 import sympy as smp # allows for symbolic manipulation
+
+
+#           --- Biomechanical Parameters ---
+etau = 100*1e4; # [1/m^2] Non-linear spring coefficient for the upper mass
+etal = 100*1e4; # [1/m^2] Non-linear spring coefficient for the lower mass
+etab = 100*1e4; # [1/m^2] Non-linear spring coefficient for the body mass
+zetau = 0.6; # 0.4; % [-] Basic damping factor for the upper mass
+zetal = 0.1; # 0.4; % [-] Basic damping factor for the lower mass
+zetab = 0.15; # 0.2; % [-] Damping factor for the body mass
+zetauCol = 1.0; # 0.4; % [-] Collision damping factor for the upper mass
+zetalCol = 1.0; # 0.4; % [-] Collision damping factor for the lower mass
+
+
+mu = 0; # [kg] Mass of the upper cover mass
+ml = 0; # [kg] Mass of the lower cover mass
+mb = 0; # [kg] Mass of the body mass
+xu0 = 0; # [m] Initial position of upper mass
+xl0 = 0; # [m] Initial position of lower mass
+xb0 = 3e-3; # [m] Initial position of body mass
+ku = 0; # [N/m] Linear spring constant for the upper mass
+kl = 0; # [N/m] Linear spring constant for the lower mass
+kc = 0; # [N/m] Coupling spring constant for the cover layer
+kb = 0; # [N/m] Linear spring constant for the body mass
+
 
